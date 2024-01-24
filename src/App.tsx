91dac9a9737f56b339baa08c0a9180ceb86d69f9
aindriu80 <aindriu80@gmail.com>
2024-01-24 13:31:59 +0000

    Creating some providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
-import { Refine, GitHubBanner, WelcomePage } from '@refinedev/core'
-import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools'
-import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
+import { Refine, GitHubBanner, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { useNotificationProvider } from '@refinedev/antd'
-import '@refinedev/antd/dist/reset.css'
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from '@refinedev/nestjs-query'
-import { createClient } from 'graphql-ws'
-import { App as AntdApp } from 'antd'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { dataProvider, liveProvider } from "./providers";
+
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import routerBindings, {
   UnsavedChangesNotifier,
   DocumentTitleHandler,
-} from '@refinedev/react-router-v6'
-import { useTranslation } from 'react-i18next'
-const API_URL = 'https://api.nestjs-query.refine.dev/graphql'
-const WS_URL = 'wss://api.nestjs-query.refine.dev/graphql'
+} from "@refinedev/react-router-v6";
 
-const gqlClient = new GraphQLClient(API_URL)
-const wsClient = createClient({ url: WS_URL })
+import { useTranslation } from "react-i18next";
 
 function App() {
-  const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation();
 
   const i18nProvider = {
     translate: (key: string, params: object) => t(key, params),
     changeLocale: (lang: string) => i18n.changeLanguage(lang),
     getLocale: () => i18n.language,
-  }
+  };
 
   return (
     <BrowserRouter>
@@ -39,8 +33,8 @@ function App() {
         <AntdApp>
           <DevtoolsProvider>
             <Refine
-              dataProvider={dataProvider(gqlClient)}
-              liveProvider={liveProvider(wsClient)}
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
               i18nProvider={i18nProvider}
@@ -48,9 +42,10 @@ function App() {
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
                 useNewQueryKeys: true,
-                projectId: 'aYMXxT-gdGW3p-b0PGdK',
-                liveMode: 'auto',
-              }}>
+                projectId: "aYMXxT-gdGW3p-b0PGdK",
+                liveMode: "auto",
+              }}
+            >
               <Routes>
                 <Route index element={<WelcomePage />} />
               </Routes>
@@ -63,7 +58,7 @@ function App() {
         </AntdApp>
       </RefineKbarProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

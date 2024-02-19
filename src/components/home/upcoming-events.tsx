1c0547bd19fce4153df2f50e0d7488c01987bdf5
aindriu80 <aindriu80@gmail.com>
2024-02-19 14:52:04 +0000

    Dashboard - continued

---
+++ src/components/home/upcoming-events.tsx
@@ -0,0 +1,11 @@
+import { Card } from "antd";
+
+const UpcomingEvents = () => {
+  return (
+    <Card style={{ height: "100%" }} headStyle={{ padding: "8px 16px" }}>
+      UpcomingEvents
+    </Card>
+  );
+};
+
+export default UpcomingEvents;

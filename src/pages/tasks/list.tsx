60016b2830fe26489f8125ea41d54908f856ebd4
aindriu80 <aindriu80@gmail.com>
2024-03-04 14:53:14 +0000

    Kanhan item

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -4,14 +4,50 @@ import {
 } from "@/components/tasks/kanban/board";
 import KanbanColumn from "@/components/tasks/kanban/column";
 import KanbanItem from "@/components/tasks/kanban/item";
+import { TASKS_QUERY, TASK_STAGES_QUERY } from "@/graphql/queries";
+import { useList } from "@refinedev/core";
 
 const List = () => {
+  const { data: stages, isLoading: isLoadingStages } = useList({
+    resource: "taskStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["TODO", "IN PROGRESS", "IN REVIEW", "DONE"],
+      },
+    ],
+    sorters: [
+      {
+        field: "createdAt",
+        order: "asc",
+      },
+    ],
+    meta: {
+      gqlQuery: TASK_STAGES_QUERY,
+    },
+  });
+  const { data: tasks, isLoading: isLoadingTasks } = useList({
+    resource: "tasks",
+    sorters: [
+      {
+        field: "dueDate",
+        order: "asc",
+      },
+    ],
+    pagination: {
+      mode: "off",
+    },
+    meta: {
+      gqlQuery: TASKS_QUERY,
+    },
+  });
   return (
     <>
       <KanbanBoardContainer>
         <KanbanBoard>
           <KanbanColumn>
-            <KanbanItem></KanbanItem>
+            <KanbanItem id=""> This is my first ToDO</KanbanItem>
           </KanbanColumn>
           <KanbanColumn></KanbanColumn>
         </KanbanBoard>

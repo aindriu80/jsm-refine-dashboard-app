110ab8074468c8fdb88e6d44e70de937c4444ae9
aindriu80 <aindriu80@gmail.com>
2024-03-06 14:49:55 +0000

    Starting the card

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { Text } from "@/components/text";
+import { User } from "@/graphql/schema.types";
+import { EyeOutlined } from "@ant-design/icons";
+import { Button, Card, ConfigProvider, Dropdown, theme } from "antd";
+import { MenuProps } from "antd/lib";
+import { useMemo } from "react";
 
-const ProjectCard = () => {
-  return <div>ProjectCard </div>;
+type ProjectCardProps = {
+  id: string;
+  title: string;
+  updatedAt: string;
+  dueDate?: string;
+  users?: {
+    id: string;
+    name: string;
+    avatarUrl?: User["avatarUrl"];
+  }[];
+};
+const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
+  const { token } = theme.useToken();
+  const edit = () => {};
+  const dropdownItems = useMemo(() => {
+    const dropdownItems: MenuProps["items"] = [
+      {
+        label: "View card",
+        key: "1",
+        icon: <EyeOutlined />,
+        onClick: () => {
+          edit();
+        },
+      },
+      {
+        danger: true,
+        label: "Delete card",
+        key: "2",
+        onClick: () => {},
+      },
+    ];
+    return dropdownItems;
+  }, []);
+
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Tag: { colorText: token.colorTextSecondary },
+          Card: { headerBg: "transparent" },
+        },
+      }}
+    >
+      <Card
+        size="small"
+        title={<Text ellipsis={{ tooltip: title }}>{title}</Text>}
+        onClick={() => edit()}
+        extra={
+          <Dropdown trigger={["click"]} menu={{ items: dropdownItems }}>
+            <Button></Button>
+          </Dropdown>
+        }
+      ></Card>
+    </ConfigProvider>
+  );
 };
 
 export default ProjectCard;

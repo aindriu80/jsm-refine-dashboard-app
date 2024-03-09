e54559aa234e43df1d087c0d562ea2b1495cb9bf
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-03-09 16:52:52 +0000

    Adding drag & drop functionality

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -8,10 +8,12 @@ import {
 import ProjectCard, { ProjectCardMemo } from "@/components/tasks/kanban/card";
 import KanbanColumn from "@/components/tasks/kanban/column";
 import KanbanItem from "@/components/tasks/kanban/item";
+import { UPDATE_TASK_STAGE_MUTATION } from "@/graphql/mutations";
 import { TASKS_QUERY, TASK_STAGES_QUERY } from "@/graphql/queries";
 import { TaskStage } from "@/graphql/schema.types";
 import { TasksQuery } from "@/graphql/types";
-import { useList } from "@refinedev/core";
+import { DragEndEvent } from "@dnd-kit/core";
+import { useList, useUpdate } from "@refinedev/core";
 import { GetFieldsFromList } from "@refinedev/nestjs-query";
 import React from "react";
 
@@ -56,6 +58,8 @@ const List = ({ children }: React.PropsWithChildren) => {
     },
   });
 
+  const { mutate: updateTask } = useUpdate();
+
   const taskStages = React.useMemo(() => {
     if (!tasks?.data || !stages?.data) {
       return {
@@ -84,6 +88,31 @@ const List = ({ children }: React.PropsWithChildren) => {
 
   const handleAddCard = (args: { stageId: string }) => {};
 
+  const handleOnDragEnd = (event: DragEndEvent) => {
+    let stageId = event.over?.id as undefined | string | null;
+    const taskId = event.active.id as string;
+    const taskStageId = event.active.data.current?.stageId;
+
+    if (taskStageId === stageId) return;
+
+    if (stageId === "unassigned") {
+      stageId = null;
+    }
+
+    updateTask({
+      resource: "tasks",
+      id: taskId,
+      values: {
+        stageId: stageId,
+      },
+      successNotification: false,
+      mutationMode: "optimistic",
+      meta: {
+        gqlMutation: UPDATE_TASK_STAGE_MUTATION,
+      },
+    });
+  };
+
   const isLoading = isLoadingStages || isLoadingTasks;
 
   if (isLoading) return <PageSkeleton />;
@@ -91,7 +120,7 @@ const List = ({ children }: React.PropsWithChildren) => {
   return (
     <>
       <KanbanBoardContainer>
-        <KanbanBoard>
+        <KanbanBoard onDragEnd={handleOnDragEnd}>
           <KanbanColumn
             id="unassigned"
             title={"unassigned"}
